b5327704aeaed0352613ae20e1b41c267e525a26
nicolas <nicolas@assouline.dev>
2024-06-08 01:14:22 +0200

    Add ability to cycle through tokens

--- scripts/mobile-token-movement-controls.js
+++ scripts/mobile-token-movement-controls.js
@@ -1,105 +1,109 @@
 export class MobileTokenMovementControls extends Application {
-  constructor(options = {}) {
-    super(options)
-  }
+    tokenCycleIndex = 0
 
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      template: `./modules/mobile-token-movement/templates/mobile-token-movement-controls.html`,
-      popOut: false,
-    })
-  }
-
-  getToken() {
-    let token = canvas.tokens.controlled[0]
-    // token.control({ releaseOthers: true })
-    // token.border.visible = false
-    return token
-  }
-
-  async move(x, y) {
-    let t = this.getToken()
-
-    let newX = t.x + t.w * x
-    let newY = t.y + t.h * y
-    // const newPoint = canvas.grid.getSnappedPosition(newX, newY)
-    const newPoint = { x: newX, y: newY}
-    console.info(`moving token from (${t.x}, ${t.y}) to (${newX}, ${newY}), ${t.checkCollision(newPoint)}`)
-    if (!t.checkCollision(newPoint)) {
-      console.info('1')
-      await t.document.update(newPoint)
-      console.info('2')
-      // canvas.animatePan({
-      //   duration: 250,
-      //   x: Math.round(newPoint.x + t.w / 2),
-      //   y: Math.round(newPoint.y + t.h / 2),
-      //   scale: canvas.scene._viewPosition.scale,
-      // })
-      await canvas.recenter();
-      console.info('3')
+    constructor(options = {}) {
+        super(options)
     }
-  }
 
-  selectToken() {
-    let t = this.getToken()
-    canvas.animatePan({
-      duration: 150,
-      x: t.x + t.w / 2,
-      y: t.y + t.h / 2,
-      scale: canvas.scene._viewPosition.scale,
-    })
-  }
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            template: `./modules/mobile-token-movement/templates/mobile-token-movement-controls.html`,
+            popOut: false,
+        })
+    }
 
-  async zoomIn() {
-    const view = canvas.scene._viewPosition
-    await canvas.animatePan({
-      duration: 200,
-      x: view.x,
-      y: view.y,
-      scale: view.scale * 1.25,
-    })
-  }
+    getToken() {
+        if (canvas.tokens.controlled.length === 0) {
+            ui.notifications.warn('No tokens selected - Please select at least one');
+            return
+        }
 
-  async zoomOut() {
-    const view = canvas.scene._viewPosition
-    await canvas.animatePan({
-      duration: 200,
-      x: view.x,
-      y: view.y,
-      scale: view.scale * 0.80,
-    })
-  }
+        return canvas.tokens.controlled[this.tokenCycleIndex]
+    }
 
-  moveTopLeft = async () => this.move(-1, -1);
+    async move(x, y) {
+        let t = this.getToken()
+        if (!t) return
 
-  moveLeft = async () => this.move(-1, 0);
+        const newPoint = {x: t.x + t.w * x, y: t.y + t.h * y}
+        console.info(`Attempting to move token from (${t.x}, ${t.y}) to ${JSON.stringify(newPoint)}, Collision: ${t.checkCollision(newPoint)}`)
 
-  moveBottomLeft = async () => this.move(-1, 1);
+        if (!t.checkCollision(newPoint)) {
+            await t.document.update(newPoint)
+            await canvas.animatePan({
+                duration: 250,
+                x: Math.round(newPoint.x + t.w / 2),
+                y: Math.round(newPoint.y + t.h / 2),
+                scale: canvas.scene._viewPosition.scale,
+            })
+        }
+    }
 
-  moveTop = async () => this.move(0, -1);
+    async selectToken() {
+        if (++this.tokenCycleIndex >= canvas.tokens.controlled.length) {
+            this.tokenCycleIndex = 0
+        }
 
-  moveBottom = async () => this.move(0, 1);
+        let t = this.getToken()
+        if (!t) return
 
-  moveTopRight = async () => this.move(1, -1);
+        await canvas.animatePan({
+            duration: 150,
+            x: t.x + t.w / 2,
+            y: t.y + t.h / 2,
+            scale: canvas.scene._viewPosition.scale,
+        })
+    }
 
-  moveRight = async () => this.move(1, 0);
+    async zoomIn() {
+        const view = canvas.scene._viewPosition
+        await canvas.animatePan({
+            duration: 200,
+            x: view.x,
+            y: view.y,
+            scale: view.scale * 1.25,
+        })
+    }
 
-  moveBottomRight = async () => this.move(1, 1);
+    async zoomOut() {
+        const view = canvas.scene._viewPosition
+        await canvas.animatePan({
+            duration: 200,
+            x: view.x,
+            y: view.y,
+            scale: view.scale * 0.80,
+        })
+    }
 
-  activateListeners(html) {
-    super.activateListeners(html)
+    moveTopLeft = async () => this.move(-1, -1);
 
-    $('.mtmc-select', html).on('click', this.selectToken)
-    $('.mtmc-zoomin', html).on('click', this.zoomIn)
-    $('.mtmc-zoomout', html).on('click', this.zoomOut)
-    $('.mtmc-topleft', html).on('click', this.moveTopLeft)
-    $('.mtmc-left', html).on('click', this.moveLeft)
-    $('.mtmc-bottomleft', html).on('click', this.moveBottomLeft)
-    $('.mtmc-top', html).on('click', this.moveTop)
-    $('.mtmc-bottom', html).on('click', this.moveBottom)
-    $('.mtmc-topright', html).on('click', this.moveTopRight)
-    $('.mtmc-right', html).on('click', this.moveRight)
-    $('.mtmc-bottomright', html).on('click', this.moveBottomRight)
+    moveLeft = async () => this.move(-1, 0);
 
-  }
+    moveBottomLeft = async () => this.move(-1, 1);
+
+    moveTop = async () => this.move(0, -1);
+
+    moveBottom = async () => this.move(0, 1);
+
+    moveTopRight = async () => this.move(1, -1);
+
+    moveRight = async () => this.move(1, 0);
+
+    moveBottomRight = async () => this.move(1, 1);
+
+    activateListeners(html) {
+        super.activateListeners(html)
+
+        $('.mtmc-select', html).on('click', this.selectToken)
+        $('.mtmc-zoomin', html).on('click', this.zoomIn)
+        $('.mtmc-zoomout', html).on('click', this.zoomOut)
+        $('.mtmc-topleft', html).on('click', this.moveTopLeft)
+        $('.mtmc-left', html).on('click', this.moveLeft)
+        $('.mtmc-bottomleft', html).on('click', this.moveBottomLeft)
+        $('.mtmc-top', html).on('click', this.moveTop)
+        $('.mtmc-bottom', html).on('click', this.moveBottom)
+        $('.mtmc-topright', html).on('click', this.moveTopRight)
+        $('.mtmc-right', html).on('click', this.moveRight)
+        $('.mtmc-bottomright', html).on('click', this.moveBottomRight)
+    }
 }
