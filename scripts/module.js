a48e338750e261df455f43b1dff8598108a5477f
nicolas <nicolas@assouline.dev>
2024-06-08 02:03:11 +0200

    Min width configurable through settings

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,24 @@
 import { MobileTokenMovementControls } from './mobile-token-movement-controls.js'
 
+const moduleName = "mobile-token-movement";
+const minWidthSettingName = 'min-width'
+
+Hooks.once('init', () => {
+	game.settings.register(moduleName, minWidthSettingName, {
+		name: "Minimum screen width (px)",
+		hint: "The minimum screen width in pixels to trigger the mobile UI",
+		scope: "world",
+		config: true,
+		default: 600,
+		type: Number,
+        requiresReload: true
+	});
+})
+
 Hooks.once('ready', () => {
-  const isMobileDevice = game.canvas.app.screen.width <= 600 || game.canvas.app.screen.height <= 600
+  const minScreenWidth = game.settings.get(moduleName, minWidthSettingName)
+
+  const isMobileDevice = game.canvas.app.screen.width <= minScreenWidth
   new Dialog({
     title: 'Mobile Token Movement',
     content: game.i18n.localize('MobileTokenMovement.dialog.content'),
